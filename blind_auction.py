8f87c94be29f42e118af14811e8464702c1d74d5
mazzi2023 <mazilugabriel2009@gmail.com>
2024-05-27 09:57:37 +0300

    .

--- blind_auction.py
+++ blind_auction.py
@@ -1,28 +1,3 @@
-programming_dictionary = {
-    "Bug": "An error in a program that prevents the program from running as expected.", 
-    "Function": "A piece of code that you can easily call over and over again.",
-    }
-#retrieving items from dictionary.
-print(programming_dictionary["Bug"])
-
-#adding new item to dictionary
-programming_dictionary["Loop"] = "The action of doing somethin ove and over again."
-print(programming_dictionary)
-
-# #create new empty dictionary
-empty_dictionary = {}
-empty_dictionary["Loop"] = "The action of doing somethin ove and over again."
-print(empty_dictionary["Loop"])
-
-# #wipe an existing dictionary
-empty_dictionary = {}
-print(empty_dictionary)
-
-# #edit an item in a dictionary
-empty_dictionary["Loop"] = "different value"
-print(empty_dictionary["Loop"])
-
-#loop through a dictionary
-for key in programming_dictionary:
-    print(key)
-    print(programming_dictionary[key])
+from replit import clear
+from art import logo
+print (logo)
